d030a4eeed4c85fcc2ccb8700aecfa47a30d74c1
Daniel Oaks <daniel@danieloaks.net>
2014-11-17 08:04:46 +1000

    Actually install thread-archiver script

--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ setup(
     author='Lawrence Wu <sagnessagiel@gmail.com>, Daniel Oaks <daniel@danieloaks.net>',
     author_email='sagnessagiel@gmail.com',
     url='https://github.com/DanielOaks/BASC-Archiver',
+    scripts=['thread-archiver'],
     packages=['basc_archiver', 'basc_archiver.sites'],
     package_dir={
         'basc_archiver': 'basc_archiver',
